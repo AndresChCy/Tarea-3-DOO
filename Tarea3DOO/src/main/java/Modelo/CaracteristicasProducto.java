f2607af3bcbfc1e7f4559c15b3cab4052ba51ece
Bandido209 <estebananm2005@gmail.com>
2024-05-29 19:45:28 -0400

    Refactorizando Expendedor, modificando logica y añadiendo depositos.

--- Tarea3DOO/src/main/java/Modelo/CaracteristicasProducto.java
+++ Tarea3DOO/src/main/java/Modelo/CaracteristicasProducto.java
@@ -1,14 +1,40 @@
 package Modelo;
 
 /**
- * Enumeración que representa diferentes productos con sus características asociadas.
+ * Enumeración que representa diferentes productos con sus características y métodos asociados.
  */
 public enum CaracteristicasProducto {
-    SPRITE(400),
-    COCACOLA(450),
-    FANTA(500),
-    SUPER8(800),
-    SNICKERS(1000);
+    SPRITE(400) {
+        @Override
+        public Bebida createProducto(int serie) {
+            return new Sprite(serie);
+        }
+    },
+    COCACOLA(450) {
+        @Override
+        public Bebida createProducto(int serie) {
+            return new CocaCola(serie);
+        }
+    },
+    FANTA(500) {
+        @Override
+        public Bebida createProducto(int serie) {
+            return new Fanta(serie);
+        }
+    },
+    SUPER8(800) {
+        @Override
+        public Dulce createProducto(int serie) {
+            return new Super8(serie);
+        }
+    },
+    SNICKERS(1000) {
+        @Override
+        public Dulce createProducto(int serie) {
+            return new Snicker(serie);
+        }
+    };
+
     /**
      * Precio asociado al producto.
      */
@@ -29,4 +55,10 @@ public enum CaracteristicasProducto {
         return precio;
     }
 
+    /**
+     * Método que crea un producto con un número de serie y lo retorna.
+     * @param serie El número de serie del producto al momento de crearse.
+     * @return      Un objeto producto creado con un número de serie especifico.
+     */
+    public abstract Producto createProducto(int serie);
 }
--- Tarea3DOO/src/main/java/Modelo/Expendedor.java
+++ Tarea3DOO/src/main/java/Modelo/Expendedor.java
@@ -1,103 +1,77 @@
 package Modelo;
+import java.util.ArrayList;
 
 /**
  * Un expendedor al que le puedes comprar productos usando monedas.
  */
 class Expendedor {
-    /** Creamos un deposito para cada elemento necesario para el expendedor */
-    private Deposito<Producto> coca;
-    private Deposito<Producto> sprite;
-    private Deposito<Producto> fanta;
-    private Deposito<Producto> snickers;
-    private Deposito<Producto> super8;
-    private Deposito<Moneda> monedero;
+    /** Creamos una lista de depositos, un deposito para monedas, un deposito unico y un deposito especial*/
+    private ArrayList<Producto>[] depositos;
+    private Deposito<Moneda> DepositoVuelto;
+    private Deposito<Moneda> DepositoMonedasUnico;
+    private Producto DepositoEspecial;
 
     /** Metodo constructor que rellena sus depositos para productos de forma magica */
-    public Expendedor(int NumProductos){
-        monedero = new Deposito<>();
-        coca = new Deposito<>();
-        sprite = new Deposito<>();
-        fanta = new Deposito<>();
-        snickers = new Deposito<>();
-        super8 = new Deposito<>();
-        for(int i=0 ; i<NumProductos;i++){
-            coca.addObject(new CocaCola(100+i));
-            sprite.addObject(new Sprite(200+i));
-            fanta.addObject(new Fanta(300+i));
-            snickers.addObject(new Snicker(400+i));
-            super8.addObject(new Super8(500+i));
+    public Expendedor(int NumProductos) {
+        DepositoVuelto = new Deposito<>();
+        depositos = new ArrayList[CaracteristicasProducto.values().length];
+        for (CaracteristicasProducto Producto : CaracteristicasProducto.values()) {
+            depositos[Producto.ordinal()] = new ArrayList<>();
+            for ( int i=0 ; i<NumProductos ; i++ ) {
+                depositos[Producto.ordinal()].add(Producto.createProducto(i));
+            }
         }
     }
 
     /**
      * Realiza la compra de un producto utilizando una moneda dada y especificaciones del producto.
-     * @param m               La moneda utilizada para la compra.
-     * @param cualProducto    El indice del producto que se desea comprar +1 para que sea mas intuitivo
-     * @return El producto comprado, si la transacción es exitosa.
+     * @param m                           La moneda utilizada para la compra.
+     * @param cualProducto                Enumeración del producto que se desea comprar.
      * @throws PagoIncorrectoException    Si la moneda pasada como parámetro es nula.
      * @throws NoHayProductoException     Si las características del producto son nulas o el producto deseado no está disponible.
      * @throws PagoInsuficienteException  Si la moneda pasada como parámetro no tiene un valor suficiente para comprar el producto.
      */
-    public Producto comprarProducto(Moneda m, int cualProducto) throws PagoIncorrectoException,NoHayProductoException,PagoInsuficienteException{
+    public void comprarProducto(Moneda m, CaracteristicasProducto cualProducto) throws PagoIncorrectoException,NoHayProductoException,PagoInsuficienteException{
         if (m==null) {
             throw new PagoIncorrectoException("Pago Insuficiente.");
         }
 
-        if ( cualProducto <= 0 || 5 < cualProducto ) {
-            monedero.addObject(m);
+        if ( cualProducto == null ) {
+            DepositoVuelto.addObject(m);
             throw new NoHayProductoException("No tenemos este producto.");
         }
 
-        Producto p;
-
-        /** Caso especifico donde la moneda tiene un valor menor al precio establecido del producto.
-         *  En cuya situación se le devuelve la misma moneda al comprador.
-         *  Caso contrario, el programa trabaja con una serie de casos para darle al usuario
-         *  el producto pedido. */
-        if (m.compareTo(CaracteristicasProducto.values()[cualProducto-1].getPrecio()) < 0) {
-            monedero.addObject(m);
+        int j = cualProducto.ordinal();
+        if (m.getValor()<cualProducto.getPrecio()) {
+            DepositoVuelto.addObject(m);
             throw new PagoInsuficienteException("Pago Insuficiente.");
-        } else {
-            switch (cualProducto) {
-                case 1 :
-                    p = sprite.getObject();
-                    break;
-                case 2:
-                    p = coca.getObject();
-                    break;
-                case 3:
-                    p = fanta.getObject();
-                    break;
-                case 4:
-                    p = super8.getObject();
-                    break;
-                case 5:
-                    p = snickers.getObject();
-                    break;
-                default:
-                    monedero.addObject(m);
-                    throw new NoHayProductoException("No tenemos este producto.");
-            }
-
-            /** En caso de que queden productos los cuales entregarle al comprador, el programa llena el monedero con monedas de 100.
-             *  Caso contrario, se lanza una excepción explicando la situación.
-             */
-            if (p!=null) {
-                for (int i = 0; i < m.compareTo(CaracteristicasProducto.values()[cualProducto-1].getPrecio()); i += 100) {
-                    monedero.addObject(new Moneda100());
-                }
-                m = null;
-            }
-            else {
-                monedero.addObject(m);
-                throw new NoHayProductoException("Lo sentimos, no hay stock de este producto en este momento.");
-            }
-            return p;
         }
+
+        if (depositos[cualProducto.ordinal()].isEmpty()) {
+            DepositoVuelto.addObject(m);
+            throw new NoHayProductoException("Lo sentimos, no hay stock de este producto en este momento.");
+        }
+
+        DepositoMonedasUnico.addObject(m);
+        for (int i = 0; i < m.compareTo(cualProducto.getPrecio()); i += 100) {
+            DepositoVuelto.addObject(new Moneda100(10+i));
+        }
+        m = null;
+
+        DepositoEspecial = (depositos[j].remove(depositos[j].size()-1));
     }
 
     /** Método get que sirve para retirar el dinero dentro del monedero. */
     public Moneda getVuelto(){
-        return monedero.getObject();
+        return DepositoVuelto.getObject();
+    }
+
+    /** Método get que sirve para retirar el producto del deposito especial. */
+    public Producto getProducto() {
+        if (DepositoEspecial!=null ) {
+            return DepositoEspecial;
+        } else {
+            return null;
+        }
     }
 }
