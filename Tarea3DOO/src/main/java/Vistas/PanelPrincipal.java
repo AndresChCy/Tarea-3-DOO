aaae33399e1937315287fcdb480ccf02d3173954
Bastian Bernal <bbernal2022@udec.cl>
2024-05-29 20:01:48 -0400

    Fix: Cambio de proporción de panel Principal y su Layout

--- Tarea3DOO/src/main/java/Vistas/PanelPrincipal.java
+++ Tarea3DOO/src/main/java/Vistas/PanelPrincipal.java
@@ -1,7 +1,6 @@
 package Vistas;
 import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.Color;
+import java.awt.*;
 
 
 public class PanelPrincipal extends JPanel {
@@ -9,12 +8,26 @@ public class PanelPrincipal extends JPanel {
     private PanelExpendedor exp;
 
     public PanelPrincipal() {
-        setLayout(null); // Utilizar layout nulo para posiciones absolutas
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH; // Permitir que los componentes se expandan
+        gbc.weightx = 1.0; // Permitir expansión horizontal
+        gbc.weighty = 1.0; // Permitir expansión vertical
+
         exp = new PanelExpendedor();
         com = new PanelComprador();
+
         this.setBackground(Color.white);
-        this.add(exp);
-        this.add(com);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0.6; // Relación de expansión horizontal
+        this.add(exp, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 0.4; // Relación de expansión horizontal
+        this.add(com, gbc);
 
     }
 }
